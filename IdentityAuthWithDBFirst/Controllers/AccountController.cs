359a93c3ec1b8a834d74cd5ad795a4d3b2d3a180
Deepak Sahani <sahani.deepak11@gmail.com>
2023-10-12 20:08:00 +0530

    implemented register, login and logout

---
+++ IdentityAuthWithDBFirst/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using IdentityAuthWithDBFirst.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IdentityAuthWithDBFirst.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AspNetUser> _userManager;
+        private readonly SignInManager<AspNetUser> _signInManager;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public AccountController(ILogger<WeatherForecastController> logger,
+            UserManager<AspNetUser> userManager,
+            SignInManager<AspNetUser> signInManager,
+            RoleManager<AspNetRole> roleManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("Register")]        
+        public async Task<IActionResult> Register(AspNetUser reqUser, string password)
+        {
+            //should have used another model object instead of AspNetUser
+
+            var user = await _userManager.FindByEmailAsync(reqUser.Email);
+            if(user != null) return Ok(new { status = false, message = "user already exists." });
+
+            //for mapping it should have AutoMapper
+            user = new AspNetUser()
+                        {
+                            FirstName = reqUser.FirstName,
+                            LastName = reqUser.LastName,
+                            Email = reqUser.Email,
+                            EmailConfirmed = reqUser.EmailConfirmed,
+                            UserName = reqUser.UserName
+                        };
+
+            var newusersres = await _userManager.CreateAsync(user, password);
+            if(newusersres.Succeeded) return Ok(new { status = false, message = "failed to  create user." });
+
+            return Ok(new { status = true, message = "user created successfully." });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Login")]        
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var res = await _signInManager.PasswordSignInAsync(user, password, false, false);
+            return Ok(res);
+        }
+
+        [HttpPost("Logout")]        
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok();
+        }
+    }
+}
--- IdentityAuthWithDBFirst/Program.cs
+++ IdentityAuthWithDBFirst/Program.cs
@@ -1,6 +1,48 @@
+using IdentityAuthWithDBFirst.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<TestDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddIdentity<AspNetUser, AspNetRole>()
+                .AddEntityFrameworkStores<TestDBContext>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    //options.LoginPath = "/Account/Login";
+    //options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +59,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
